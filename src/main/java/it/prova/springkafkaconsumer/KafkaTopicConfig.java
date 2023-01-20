6f4c7187959e7e37eb3eb1d6a77c3c741b579b00
rossenbergvillanuevaramboanga <rossenebrgvillanueva.ramboanga@gmail.com>
2023-01-20 10:45:09 +0100

    Creation of TopicConfig

---
+++ src/main/java/it/prova/springkafkaconsumer/KafkaTopicConfig.java
@@ -0,0 +1,30 @@
+package it.prova.springkafkaconsumer;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KafkaTopicConfig {
+
+    //File Configuration for Topic
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic1() {
+        return new NewTopic("RossTopic", 1, (short) 1);
+    }
+}
---
+++ src/test/java/it/prova/springkafkaconsumer/SpringKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.springkafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
