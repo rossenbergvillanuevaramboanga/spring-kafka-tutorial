015e95f6d5850ff51bba55e9a7072e1873da7d34
rossenbergvillanuevaramboanga <rossenebrgvillanueva.ramboanga@gmail.com>
2023-01-20 11:44:40 +0100

    Implemented basic functionality of Producer & Consumer

--- src/main/java/it/prova/springkafkaconsumer/KafkaTopicConfig.java
+++ src/main/java/it/prova/springkafkaconsumer/KafkaTopicConfig.java
@@ -4,14 +4,14 @@ import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration
 public class KafkaTopicConfig {
-
-    //File Configuration for Topic
 
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
@@ -25,6 +25,6 @@ public class KafkaTopicConfig {
 
     @Bean
     public NewTopic topic1() {
-        return new NewTopic("RossTopic", 1, (short) 1);
+        return new NewTopic("baeldung", 1, (short) 1);
     }
 }
